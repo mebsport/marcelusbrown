07235cc6f9c4b4fb5dcd405698d9082ebda4d369
mebsport <56307645+mebsport@users.noreply.github.com>
2023-01-22 19:18:14 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,6 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <p>This Site Is Currently Down For Maitnence. For Any Inquires email marcelus@marcelusbrown.com</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
